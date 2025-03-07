367b7b98e8930839878ca88cae0ae46779a6bff2
Uczen <uczen@dzdzw.local>
2025-03-07 15:38:46 +0100

    dodano zadanie 9

---
+++ zadanie 9/index.sql
@@ -0,0 +1,7 @@
+CREATE TABLE uzytkownicy (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    imie VARCHAR(100),
+    nazwisko VARCHAR(100)
+);
+INSERT INTO uzytkownicy (imie, nazwisko) VALUES ('Jan', 'Kowalski');
+INSERT INTO uzytkownicy (imie, nazwisko) VALUES ('Anna', 'Nowak');
