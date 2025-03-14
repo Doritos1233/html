8de609049acd4007aadd7cd7d73c65c0cb587cb3
Uczen <uczen@dzdzw.local>
2025-03-14 15:41:32 +0100

    dodano zadanie 9

--- zadanie 9/rezerwacja.php
+++ zadanie 9/rezerwacja.php
@@ -4,27 +4,23 @@ $username = "root";
 $password = "";
 $database = "baza";
 
-$conn = new mysqli($server, $username, $password, $database);
+$connection = new mysqli($server, $username, $password, $database);
 
 if (!$connection) {
   die($connection);
 }
-echo "Connected successfully";
 
 foreach ($_POST as $key => $value) {
   echo $key, "; ", $value, " ";
 }
 
-$Date = $_POST ["data"];
-$Phone = $_POST ["telefon"];
-$AmounOfPeople = $_POST["ilość_osób"];
+$Date = $_POST ["data_rez"];
+$Phone = $_POST ["tel"];
+$AmounOfPeople = $_POST["ilosc"];
 
-echo($date $Phone $amountOfPeople);
 
-mysqli_query(
+mysqli_query($connection, "INSERT INTO `rezerwacje`(`data_rez`, `liczba_osob`, `telefon`) VALUES ('$Date','$AmounOfPeople','$Phone')");
+ 
 
-);
-
-mysqli_close($conn);
-
+mysqli_close($connection);
 ?>
