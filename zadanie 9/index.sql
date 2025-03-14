381b1bcb6d396e33f5154fc8ee2b235a6c6b5bdf
Uczen <uczen@dzdzw.local>
2025-03-14 09:38:16 +0100

    dodano zadanie 9

--- zadanie 9/index.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE uzytkownicy (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    imie VARCHAR(100),
-    nazwisko VARCHAR(100)
-);
-INSERT INTO uzytkownicy (imie, nazwisko) VALUES ('Jan', 'Kowalski');
-INSERT INTO uzytkownicy (imie, nazwisko) VALUES ('Anna', 'Nowak');
--- zadanie 9/rezerwacja.php
+++ zadanie 9/rezerwacja.php
@@ -1,13 +1,30 @@
 <?php
-$servername = "localhost";
+$server = "localhost";
 $username = "root";
 $password = "";
 $database = "baza";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($server, $username, $password, $database);
 
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if (!$connection) {
+  die($connection);
 }
 echo "Connected successfully";
+
+foreach ($_POST as $key => $value) {
+  echo $key, "; ", $value, " ";
+}
+
+$Date = $_POST ["data"];
+$Phone = $_POST ["telefon"];
+$AmounOfPeople = $_POST["ilość_osób"];
+
+echo($date $Phone $amountOfPeople);
+
+mysqli_query(
+
+);
+
+mysqli_close($conn);
+
 ?>
