c1653ac5b562dc2b4ce73ceade25d8f61cb8701c
Uczen <uczen@dzdzw.local>
2025-05-09 16:43:38 +0200

    dodano zadegzamin

---
+++ zadegzamin/html/wedkuj.php
@@ -0,0 +1,19 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "wedkowanie";
+
+$connection = mysqli_connect($server, $username, $password, $database);
+
+if (!$connection) {
+    die($connection);
+}
+
+$query1 = "SELECT `id`, `nazwa`, `wystepowanie` FROM `ryby` WHERE ryby.styl_zycia = 2";
+$result1 = mysqli_query($connection, $query1);
+
+while ($row = mysqli_fetch_array($result1)) {
+    echo "<li>$row[0] pływa w rzece $row[1] wojewodztwo $row[2]</li>";
+}
+?>
