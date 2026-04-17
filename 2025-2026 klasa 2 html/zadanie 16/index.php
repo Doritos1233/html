54224551d1cd84f048c1152c7028f1145ed1ef6e
Jest_zle <lol@lol.pl>
2026-04-17 10:49:44 +0200

    dodano

---
+++ 2025-2026 klasa 2 html/zadanie 16/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST">
+        <textarea name="text"></textarea>
+        <button type="submit">Wyślij</button>
+    </form>
+    <?php
+
+    if (isset($_POST['text'])) {
+        echo $_POST['text'];
+        $now = date("[ Y-m-d H:i:s ]") . " - ";
+
+        $file = fopen("log.txt", "a");
+        fwrite($file, $now . $_POST['text'] . "\n");
+        fclose($file);
+    }
+    ?>
+</body>
+
+</html>
